ecbd659df4f6852dcb6a7a7890b5d58ec6f87215
Christiaan Westerbeek <chris@devotis.nl>
2014-01-03 21:16:39 +0100

    Fixes to show results after all CRUD functions

--- examples/app.js
+++ examples/app.js
@@ -10,16 +10,50 @@ clang.init('7e5b4df2-7a91-4af0-9119-1fc2d6fdc8a2', '1.18', function(err, api) {
 
 	/*api.objects.group.getAll({}, function(err, rows) {
 
-		console.log('errhere', err, rows);
+		console.log(err, rows);
 
 	});*/
-	api.objects.customer.getByObject({
+	/*api.objects.customer.getByObject({
 		customer: {
 			emailAddress: 'c.westerbeek@gmail.com'
 		}
 	}, function(err, rows) {
 
 		console.log('errhere', err, rows);
+
+	});*/
+	/*api.objects.customer.insert({
+		customer: {
+			firstname: 'Christiaan',
+			emailAddress: 'c.westerbeek@gmail.com'
+		}
+	}, function(err, rows) {
+
+		console.log(err, rows);
+
+	});*/
+
+	/*api.objects.customer.update({
+		customer: {
+			id: 39514,
+			firstname: 'Christiaan333',
+			emailAddress: 'c.westerbeek@gmail.com'
+		}
+	}, function(err, rows) {
+
+		console.log(err, rows);
+
+	});*/
+
+	api.objects.customer.delete({
+		customer: {
+			id: 39514,
+			firstname: 'Christiaan333',
+			emailAddress: 'c.westerbeek@gmail.com'
+		}
+	}, function(err, rows) {
+
+		console.log(err, rows);
 
 	});
 });
