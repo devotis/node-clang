1ccdfa2aa441ab8d685ed8da8a8558b251b7533a
Christiaan Westerbeek <chris@devotis.nl>
2017-07-02 21:02:45 +0200

    Separate delete-all test

--- test/customer.js
+++ test/customer.js
@@ -9,37 +9,6 @@ let clang = new Clang({version: '*', uuid: config.get('uuid'), logPayload: false
 
 const lib = require('./lib');
 
-tape('deleting all customers', function (t) {
-  async.autoInject({
-    createFirst: function(cb) {
-      clang.request('customer_insert', {customer: {emailAddress: 'a1@b.nl'}}, cb)
-    },
-    createSecond: function(cb) {
-      clang.request('customer_insert', {customer: {emailAddress: 'a2@b.nl'}}, cb)
-    },
-    getAll: ['createFirst', 'createSecond', function(createFirst, createSecond, cb) {
-      clang.request('customer_getAll', cb)
-    }],
-    deleteAll: ['getAll', function(getAll, cb) {
-      async.map(getAll, (customer, cb) => {
-        clang.request('customer_delete', {customer}, cb)
-      }, cb)
-    }],
-    getAllAgain: ['deleteAll', function(deleteAll, cb) {
-      clang.request('customer_getAll', cb)
-    }]
-  }, (err, result) => {
-    t.notOk(err, 'No error occured')
-    t.ok(lib.arrayWithObjects(result.getAll, 2, true), 'At least 2 records are retrieved as an array with objects')
-    t.ok(result.deleteAll.length >= 2, 'At least 2 records are deleted and acknowledged as an array of objects')
-    result.deleteAll.forEach(deleteResult => {
-      t.equal(deleteResult, true, '`true` is exactly the result for each deleted record')
-    })
-    t.equal(result.getAllAgain.length, 0, 'And they are gone again')
-    t.end()
-  })
-})
-
 tape('creating a customer', function (t) {
   async.autoInject({
     withoutCustomerObject: function(cb) {
