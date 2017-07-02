a43cff3798551f08e1975604ec24ecee78df1baf
Christiaan Westerbeek <chris@devotis.nl>
2017-07-02 13:58:39 +0200

    Add non failing email creation test

--- test/email.js
+++ test/email.js
@@ -28,19 +28,27 @@ tape('creating an email', function (t) {
     create: function(cb) {
       clang.request('email_create', cb)
     },
-    insert: ['create', function(create, cb) {
-      clang.request('email_insert', {email: create}, err => {
+    insertThatFails: ['create', function(create, cb) {
+      let email = Object.assign({}, create)
+
+      clang.request('email_insert', {email}, err => {
         t.ok(err, 'Can\'t create an email without a content name')
         t.equal(lib.faultcode(err), 751, 'and returns a invalid content name []')
         cb()
       })
+    }],
+    insert2: ['create', function(create, cb) {
+      let email = Object.assign({}, create, {
+        name: 'test-name',
+        htmlContent: 'test-htmlContent'
+      })
+      clang.request('email_insert', {email}, cb)
     }]
   }, (err, result) => {
-    console.log('result.create', result.create)
-    console.log('result.insert', result.insert)
     t.notOk(err, 'No error occured')
     t.ok(result.create)
     t.ok(lib.isObject(result.create), 'The create result is a non-empty object')
+    t.ok(lib.isObject(result.insert2), 'The insert result is a non-empty object')
     t.end()
   })
 })
